8f6719184da16c662665bf24c1f4751c2282b745
Shun Sakai <sorairolake@protonmail.ch>
2019-07-26 01:07:42 +0900

    Change to use features of ES6
    * Change to use "let" and "const"
    * Fix indent level

--- background.js
+++ background.js
@@ -1,27 +1,27 @@
 "use strict";
 
-var requestFilter = {
-        urls: ["https://*.twitter.com/*"]
-    },
+const requestFilter = {
+    urls: ["https://*.twitter.com/*"]
+};
 
-    extraInfoSpec = ['requestHeaders', 'blocking'],
-    // Chrome will call your listener function in response to every
-    // HTTP request
-    handler = function (details) {
+const extraInfoSpec = ['requestHeaders', 'blocking'];
+// Chrome will call your listener function in response to every
+// HTTP request
+const handler = function (details) {
 
-        var headers = details.requestHeaders,
-            blockingResponse = {};
+    let headers = details.requestHeaders;
+    let blockingResponse = {};
 
-        for (var i = 0, l = headers.length; i < l; ++i) {
-            if (headers[i].name == 'User-Agent') {
-                headers[i].value = 'Mozilla/5.0 (Windows NT 9.0; WOW64; Trident/7.0; rv:11.0) like Gecko';
-                break;
-            }
+    for (let i = 0, const l = headers.length; i < l; ++i) {
+        if (headers[i].name == 'User-Agent') {
+            headers[i].value = 'Mozilla/5.0 (Windows NT 9.0; WOW64; Trident/7.0; rv:11.0) like Gecko';
+            break;
         }
+    }
 
-        blockingResponse.requestHeaders = headers;
-        return blockingResponse;
-    };
+    blockingResponse.requestHeaders = headers;
+    return blockingResponse;
+};
 
 function removeCookie() {
     chrome.browsingData.remove({"origins": ["https://twitter.com"]}, {"cacheStorage": true, "cache": true});
