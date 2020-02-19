fb3453fe7739ac597c02533921a2772e162ec727
Tobias Messner <tobias.d.messner@gmail.com>
2020-02-19 21:23:13 +0100

    Fixed issue where banner appears during search

--- remove-banner.js
+++ remove-banner.js
@@ -3,14 +3,28 @@ let topBannerContainer = document.getElementById("banners");
 if (topBannerContainer !== null) {
     topBannerContainer.classList.add("hidden")
 }
-// Remove search warning banner
-let searchWarningContainer = document.getElementsByClassName("BannersContainer");
-if (searchWarningContainer !== null && searchWarningContainer[0] !== undefined) {
-    searchWarningContainer[0].classList.add("hidden")
-}
+
+
 // Resize the large padding removing the banner creates
 let topSpacerBar = document.getElementsByClassName("topbar-spacer");
 if (topSpacerBar !== null && topSpacerBar[0] !== undefined) {
     topSpacerBar[0].style.paddingTop = "50px";
 }
 
+
+// Remove search warning banner
+let searchWarningContainer = document.getElementsByClassName("BannersContainer");
+if (searchWarningContainer !== null && searchWarningContainer[0] !== undefined) {
+    searchWarningContainer[0].classList.add("hidden");
+}
+
+
+// make sure that if the banner doesn't exist, wait until it does before removing it
+let searchObserver = new MutationObserver(mutations => {
+    let searchWarningContainer = document.getElementsByClassName("BannersContainer");
+    if (searchWarningContainer !== null && searchWarningContainer[0] !== undefined) {
+        searchWarningContainer[0].classList.add("hidden");
+        searchObserver.disconnect();
+    }
+});
+searchObserver.observe(document.body, {childList: true, subtree: true});
